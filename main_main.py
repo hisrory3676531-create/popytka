2061a48387109bcf4ae6a500ecf39454041ed8c0
Александр Тетёра <hisrory3676531@gmail.com>
2026-09-24 00:42:52 -0500

    Update main_main.py

--- main_main.py
+++ main_main.py
@@ -17,7 +17,7 @@ except Exception as e:
     print(f"ОШИБКА ИМПОРТА: {e}", flush=True)
 
 # НАСТРОЙКИ
-TOKEN = 'vk1.a.Oa7qU9frZCB0t7zmQWa2BE4ufu4hg40cS6plGCnJwMzLdDSX2sf7RlD_o7k4nYi24679q8L1t6ivJAqkMf8OlRUm_GGbgZ-N0s9tDvvNtVFxbpFgv5mdR1d17oj3ZmsW-_-C2xRV8Y_qZa4p1rDpXBwzaPGuBSiwQnVBYSmdSew_rqMT5s14puZhNFA3qv2t4ciBAOO_dbFjNDBf6kvD1A'
+TOKEN = 'vk1.a.8GXB8a57NS6BxBO3LCa9BFNf2AQ5xSqePsQVSN4A2dagp4LlpFXywHjnbznewTQBuRJvruPHqUlQitRhGzYDXPOKzziH-VupeZ7r48voZ4O3cs7OLVmiJPHzioMeSmBRhf_1D3I8HhrxegjAFDzL0r7XRJcvDoATyqE5HXsKlESd8x13-jOv9kCVxkT5j7Lgt4Mw4iNU3DtLsCB3RDfx-g'
 ALLOWED_USER_ID = 802229179  # Твой ID
 
 # Список ваших групп (ID должны быть с минусом)
